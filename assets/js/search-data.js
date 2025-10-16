4c16bff114eeb38071f87794d19ea429fcd732e8
taehyeonkweon <66986669+taehyeonkweon@users.noreply.github.com>
2025-10-16 05:51:42 +0000

    Deploying to gh-pages from @ taehyeonkweon/taehyeonkweon.github.io@d0343690a339e1bb2f977622bbdcf882995eb1a4 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -501,14 +501,14 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%74%68%6B%77%65%6F%6E@%75%77%61%74%65%72%6C%6F%6F.%63%61", "_blank");
         },
       },{
         id: 'social-inspire',
         title: 'Inspire HEP',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://inspirehep.net/authors/", "_blank");
         },
       },{
         id: 'social-rss',
@@ -522,7 +522,7 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://scholar.google.com/citations?user=", "_blank");
         },
       },{
         id: 'social-custom_social',
