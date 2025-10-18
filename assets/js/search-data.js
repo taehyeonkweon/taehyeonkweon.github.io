8f506b8707add4f38b195a7c5176b43264706813
taehyeonkweon <66986669+taehyeonkweon@users.noreply.github.com>
2025-10-18 16:13:49 +0000

    Deploying to gh-pages from @ taehyeonkweon/taehyeonkweon.github.io@1c822bccf4ffb30c84126bb60162704d719b708d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,7 +25,7 @@ ninja.data = [{
           },
         },{id: "nav-teaching",
           title: "Teaching",
-          description: "Teaching Assistant experience at the University of Waterloo.",
+          description: "Teaching Assistant and Peer Tutor experience at the University of Waterloo and The Hong Kong Polytechnic University.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/teaching/";
