4acc703290eef8008fa04f8f01850152491a8148
taehyeonkweon <66986669+taehyeonkweon@users.noreply.github.com>
2026-06-02 20:22:33 +0000

    Deploying to gh-pages from @ taehyeonkweon/taehyeonkweon.github.io@6a0bbcba73a3ffc521626bba3bb18dbf6deb7a07 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -405,16 +405,8 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+            },},{id: "news-our-paper-semantic-aware-active-perception-for-next-best-view-grasp-planning-has-been-accepted-to-the-international-journal-of-precision-engineering-and-manufacturing-ijpem",
+          title: 'Our paper, “Semantic-Aware Active Perception for Next-Best-View Grasp Planning,” has been accepted to...',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
